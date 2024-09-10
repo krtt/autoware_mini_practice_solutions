55dc609e5cff17208073635a9c84e2a7f564ad2c
Kertu <linuxuser@linux-d2006-18.domenis.local>
2024-09-10 12:27:26 +0300

    P1 step 7

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -4,9 +4,14 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+
 pub = rospy.Publisher('/message', String, queue_size=10)
 
+message = rospy.get_param('~message', "Hello World - default")
+rate = rospy.get_param('~rate', 2)
+
+rate = rospy.Rate(rate)
+
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
